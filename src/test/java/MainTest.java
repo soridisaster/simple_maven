261ecd24afb48979d8248e4d302bf63c28886f9d
soridisaster <74194131+soridisaster@users.noreply.github.com>
2021-12-25 11:20:21 +0300

    Насрала в тесты

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -22,7 +22,7 @@ public class MainTest {
         Main.main(null);
         try {
 
-            assertEquals(3, new Main().getSum());
+            assertEquals(8, new Main().getSum());// не 8, 3
             assertEquals(4+5, new Main(4,5).getSum());
         } catch (Exception e) {
             fail(e.toString());
